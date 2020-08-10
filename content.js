4ca7758a1dc3342403958942fa6635edf0e43db5
Jonathan Arava <32462175+jonathanarava@users.noreply.github.com>
2020-08-10 19:38:29 -0400

    stable

--- content.js
+++ content.js
@@ -8,10 +8,10 @@ function replaceText(element) {
         element.childNodes.forEach(replaceText)
     } else if (element.nodeType === Text.TEXT_NODE) {
         //Coronavirus
-        if (element.textContent.match(/coronavirus/gi)) {
+        if (element.textContent.match(/(coronavirus)/gi)) { //brackets not necessary
             console.log('%c Match!', 'color: green; font-weight: bold;')
             const newElement = document.createElement('span')
-            newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi, '<span class="rainbow">$1</span>')
+            newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi, '<span class="rainbow">$1</span>') //brackets necessary (output: $1)
             element.replaceWith(newElement)
         }
     }
@@ -27,13 +27,14 @@ function gotMessage(message) {
     switch (message.status){
         case "0":
             console.log("Less COVID")
+            break;
         case "1":
             console.log("give it to me as is")
+            break;
         case "2":
             console.log("More COVID")
             replaceText(document.body)
-        default:
-            console.log(message.status)
+            break;
 
     }
 
