47d9627e477506c96c603c651c6a7d6286ba1ae3
Jonathan Arava <32462175+jonathanarava@users.noreply.github.com>
2020-08-11 13:18:04 -0400

    function replaceText uses tag words instead of static word

--- content.js
+++ content.js
@@ -3,16 +3,18 @@
 /*
     converts "element" 
 */
-function replaceText(element, tag_word) {
-    tag_word = "/(" + tag_word + ")/gi";
+
+const tag_word = /(coronavirus)/gi;
+function replaceText(element) {  
     if (element.hasChildNodes()) {
         element.childNodes.forEach(replaceText)
+        console.log(tag_word);
     } else if (element.nodeType === Text.TEXT_NODE) {
         //Coronavirus
-        if (element.textContent.match(/(coronavirus)/gi)) { //brackets not necessary
+        if (element.textContent.match(tag_word)) { //brackets not necessary
             console.log('%c Match!', 'color: green; font-weight: bold;')
             const newElement = document.createElement('span')
-            newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi, '<span class="rainbow">$1</span>') //brackets necessary (output: $1)
+            newElement.innerHTML = element.textContent.replace(tag_word, '<span class="rainbow">$1</span>') //brackets necessary (output: $1)
             element.replaceWith(newElement)
         }
     }
@@ -25,17 +27,16 @@ chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message) {
     console.log(message)
-    console.log(message.status)
 
     switch (message.status){
         case "0":
-            console.log("Less COVID")
+            //console.log("Less COVID")
             break;
         case "1":
-            console.log("give it to me as is")
+            //console.log("give it to me as is")
             break;
         case "2":
-            console.log("More COVID")
+            //console.log("More COVID")
             replaceText(document.body)
             break;
 
