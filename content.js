5425dc90f59eb164d32428ea0cc19ef627c6d845
Jonathan Arava <32462175+jonathanarava@users.noreply.github.com>
2020-08-11 16:09:52 -0400

    update

--- content.js
+++ content.js
@@ -12,7 +12,6 @@ function replaceText(element) {
     if (element.hasChildNodes()) {
         element.childNodes.forEach(replaceText)
     } else if (element.nodeType === Text.TEXT_NODE) {
-        //Coronavirus
         if (element.textContent.match(tag_word)) { //brackets not necessary
             //console.log('%c Match!', 'color: green; font-weight: bold;')
             const newElement = document.createElement('span')
@@ -22,14 +21,33 @@ function replaceText(element) {
     }
 }
 
- function max_corona(element, array){
+
+function max_corona(element, array){
     for(i=0; i< array.length; i++){
         tag_word = array[i];
         replaceText(element);
     }
- }
+}
 
-     
+ function removeText(element) {  
+    if (element.hasChildNodes()) {
+        element.childNodes.forEach(replaceText)
+    } else if (element.nodeType === Text.TEXT_NODE) {
+        if (element.textContent.match(tag_word)) { //brackets not necessary
+            const newElement = document.createElement('span')
+            newElement.innerHTML = element.textContent.replace(tag_word, '<span class="rainbow">""</span>') //brackets necessary (output: $1)
+            element.replaceWith(newElement)
+            
+        }
+    }
+}
+    
+function min_corona(element, array){
+    for(i=0; i< array.length; i++){
+        tag_word = array[i];
+        removeText(element);
+    }
+}
 
 
 
@@ -40,13 +58,14 @@ function gotMessage(message) {
 
     switch (message.status){
         case "0":
-            //console.log("Less COVID")
+            console.log("Less COVID")
+            min_corona(document.body,tag_words)
             break;
         case "1":
-            //console.log("give it to me as is")
+            console.log("give it to me as is")
             break;
         case "2":
-            //console.log("More COVID")
+            console.log("More COVID")
             max_corona(document.body,tag_words)
             break;
 
