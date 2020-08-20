c0b9d64b534eda55743840d8000f961cd4d8bfe8
Jonathan Arava <32462175+jonathanarava@users.noreply.github.com>
2020-08-20 00:21:42 -0400

    latest

--- pop/popup.js
+++ pop/popup.js
@@ -11,11 +11,33 @@ window.addEventListener('load', function load(event) {
             chrome.tabs.sendMessage(tab_id, data)
             console.log("msg sent")
         })
-      
+    
+
         MIN.addEventListener("mouseenter", function(){
-            
-            console.log("lower")
+            document.getElementById("min_description").style.display = "inline";
+            console.log("hovered over min")
         })
+
+        I.addEventListener("mouseenter", function(){
+            document.getElementById("L_description").style.display = "inline";
+            console.log("hovered over I")
+        })
+
+        MAX.addEventListener("mouseenter", function(){
+            document.getElementById("max_description").style.display = "inline";
+            console.log("hovered over max")
+        })
+
+        var elements = document.getElementsByClassName("description");
+
+        var myFunction = function(element) {
+            element[i].style.display = "inline";
+            console.log("exited over abbrevieation")
+        };
+        
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].addEventListener('mouseleave', myFunction);
+        }
 
     });
 
