c9099c1ad6cbb1a5e57a955e4327ffb2ff9da36b
Jonathan Arava <32462175+jonathanarava@users.noreply.github.com>
2020-08-23 12:35:03 -0400

    stable

--- pop/popup.js
+++ pop/popup.js
@@ -1,4 +1,5 @@
 var container = document.getElementById("container")
+var min_flag, max_flag, I_flag;
 
 window.addEventListener('load', function load(event) {
     chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
@@ -15,23 +16,41 @@ window.addEventListener('load', function load(event) {
 
         MIN.addEventListener("mouseenter", function(){
             document.getElementById("min_description").style.display = "inline";
+            min_flag = true;
             console.log("hovered over min")
         })
 
         I.addEventListener("mouseenter", function(){
             document.getElementById("L_description").style.display = "inline";
+            I_flag = true;
             console.log("hovered over I")
         })
 
         MAX.addEventListener("mouseenter", function(){
             document.getElementById("max_description").style.display = "inline";
+            max_flag = true;
             console.log("hovered over max")
         })
 
         var elements = document.getElementsByClassName("description");
 
         var myFunction = function(element) {
-            element[i].style.display = "inline";
+            //element[i].style.display = "inline";
+            if (min_flag == true){
+                console.log("min_flag");
+                document.getElementById("min_description").style.display = "none";
+                min_flag = false;
+            }
+            if (I_flag == true){
+                console.log("max_flag");
+                document.getElementById("L_description").style.display = "none";
+                I_flag = false;
+            }
+            if (max_flag == true){
+                console.log("I_flag");
+                document.getElementById("max_description").style.display = "none";
+                max_flag = false;
+            }
             console.log("exited over abbrevieation")
         };
         
